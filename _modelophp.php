2076d850f2d1736541123b53d7f97ff33ca4f1af
dansc00 <122238649+dansc00@users.noreply.github.com>
2023-01-30 23:32:52 -0300

    Add files via upload

---
+++ _modelophp.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="../css/estilo.css"/>
+	<meta charset="UTF-8"/>
+	<title>Curso de PHP - CursoemVideo.com</title>
+	<style>
+		h4{
+			font-size: 15pt;
+			margin: 0px;
+		}
+	</style>
+</head>
+<body>
+<div>
+	<?php
+			
+			
+
+				
+	?>
+</div>
+</body>
+</html>
+ 
---
+++ ola.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Primeiro exemplo PHP</title>
+    <style>
+        h2{
+            color: #4654a4;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php 
+        echo "<h2>Olá Mundo!</h2>";
+    ?>
+</body>
+</html>
